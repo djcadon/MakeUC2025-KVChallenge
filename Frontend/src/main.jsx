076f6501270ccb79cc4f93da9a478f6e904efa7a
djcadon <caden1215@ipmolders.com>
2025-11-08 15:13:56 -0500

    fixed frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
-import KineticVisionDashboard from './dashboard.jsx'
+import KineticVisionDashboard from './dashboard'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <KineticVisionDashboard />
-  </StrictMode>,
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<KineticVisionDashboard />} />
+            </Routes>
+        </Router>
+    </StrictMode>,
 )
